a7ff37d7202748a0a1350ee6018a10c2e20f0bcd
Leigh MacDonald <leigh.macdonald@gmail.com>
2021-09-19 17:11:04 -0600

    Fix addserver cli command

--- internal/cmd/addserver.go
+++ internal/cmd/addserver.go
@@ -5,20 +5,17 @@ import (
 	"github.com/leighmacdonald/gbans/internal/config"
 	"github.com/leighmacdonald/gbans/internal/model"
 	"github.com/leighmacdonald/gbans/internal/store"
-	"github.com/leighmacdonald/golib"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"time"
 )
 
-var addServer = model.Server{
-	ServerName: "",
-	Token:      "",
-	Address:    "",
-	Port:       27015,
-	RCON:       "",
-	Password:   golib.RandomString(20),
-}
+var (
+	name = ""
+	host = ""
+	port = 27015
+	rcon = ""
+)
 
 // addServerCmd represents the addserver command
 var addServerCmd = &cobra.Command{
@@ -29,33 +26,35 @@ var addServerCmd = &cobra.Command{
 		if err != nil {
 			log.Fatalf("Failed to setup db connection: %v", err)
 		}
-		if addServer.ServerName == "" {
+		if name == "" {
 			log.Fatal("Server name cannot be empty")
 		}
-		if addServer.Address == "" {
+		if host == "" {
 			log.Fatal("Server address cannot be empty")
 		}
-		if addServer.Port <= 0 || addServer.Port > 65535 {
+		if port <= 0 || port > 65535 {
 			log.Fatal("Invalid server port")
 		}
-		if addServer.RCON == "" {
-			log.Fatal("RCON password cannot be empty")
+		if rcon == "" {
+			log.Fatal("rcon password cannot be empty")
 		}
+		srv := model.NewServer(name, host, port)
+		srv.RCON = rcon
 		ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
 		defer cancel()
-		if err := db.SaveServer(ctx, &addServer); err != nil {
+		if err := db.SaveServer(ctx, &srv); err != nil {
 			log.Fatalf("Could not create server: %v", err)
 		}
-		log.Infof("Added server %s with token %s - This token must be added to your servers gbans.cfg",
-			addServer.ServerName, addServer.Password)
+		log.WithFields(log.Fields{"token": srv.Password, "name": name}).
+			Info("Added server successfully. This password must be added to your servers gbans.cfg")
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(addServerCmd)
 
-	addServerCmd.Flags().StringVarP(&addServer.ServerName, "name", "n", "", "Short server ID eg: us-1")
-	addServerCmd.Flags().StringVarP(&addServer.Address, "host", "H", "", "Server hostname/ip eg: us-1.myserver.com")
-	addServerCmd.Flags().IntVarP(&addServer.Port, "port", "p", 27015, "Server port")
-	addServerCmd.Flags().StringVarP(&addServer.RCON, "rcon", "r", "", "Server RCON password")
+	addServerCmd.Flags().StringVarP(&name, "name", "n", "", "Short server ID eg: us-1")
+	addServerCmd.Flags().StringVarP(&host, "host", "H", "", "Server hostname/ip eg: us-1.myserver.com")
+	addServerCmd.Flags().IntVarP(&port, "port", "p", 27015, "Server port")
+	addServerCmd.Flags().StringVarP(&rcon, "rcon", "r", "", "Server rcon password")
 }
