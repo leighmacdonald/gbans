9d565692e7c7cb4fc76755fa82e2b2aad866c1c6
Leigh MacDonald <leigh.macdonald@gmail.com>
2022-01-01 02:03:18 -0700

    Remove action file

--- internal/app/filters_test.go
+++ internal/app/filters_test.go
@@ -12,7 +12,7 @@ func TestIsFilteredWord(t *testing.T) {
 	l1 := len(wordFilters)
 	importFilteredWords([]model.Filter{{WordID: 1, Pattern: regexp.MustCompile("badword"), CreatedOn: time.Now()}})
 	require.Equal(t, l1+1, len(wordFilters))
-	matched, matchedFilter := gbans{}.ContainsFilteredWord("This is a badword")
+	matched, matchedFilter := ContainsFilteredWord("This is a badword")
 	require.True(t, matched)
 	require.Equal(t, 1, matchedFilter.WordID)
 }
