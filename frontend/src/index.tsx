9295a695f4fa0dc1737e67236eed3ca56864cfe4
Leigh MacDonald <leigh.macdonald@gmail.com>
2023-08-08 05:10:09 -0600

    Dont embed font

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,10 +2,6 @@ import React from 'react';
 import { App } from './App';
 import { createRoot } from 'react-dom/client';
 import { PaletteMode } from '@mui/material';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
 
 // extend window with our own items that we inject
 declare global {
