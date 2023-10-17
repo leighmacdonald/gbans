a716acb039275ac0b1c1e0ed47830e6b7f551de0
Leigh MacDonald <leigh.macdonald@gmail.com>
2023-10-17 07:14:06 -0600

    Add generic confirm modal

---
+++ frontend/src/component/modal/Confirm.tsx
@@ -0,0 +1,86 @@
+import NiceModal, { muiDialogV5, useModal } from '@ebay/nice-modal-react';
+import {
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle
+} from '@mui/material';
+import { Heading } from '../Heading';
+import { LoadingSpinner } from '../LoadingSpinner';
+import Stack from '@mui/material/Stack';
+import { CancelButton, ConfirmButton } from './Buttons';
+import React, { useCallback, useState } from 'react';
+import Typography from '@mui/material/Typography';
+import { useTheme } from '@mui/material/styles';
+import CheckIcon from '@mui/icons-material/Check';
+
+export interface ConfirmProps {
+    title?: string;
+    description?: string;
+    onError?: (error: string) => void;
+    onConfirm: () => Promise<void>;
+}
+
+export const Confirm = NiceModal.create(
+    ({ title, description, onConfirm }: ConfirmProps) => {
+        const [inProgress, setInProgres] = useState(false);
+        const [error, setError] = useState('');
+        const modal = useModal();
+        const theme = useTheme();
+
+        const defaultStartDate = new Date();
+        const defaultEndDate = new Date();
+        defaultEndDate.setDate(defaultStartDate.getDate() + 1);
+
+        const onSubmit = useCallback(async () => {
+            setInProgres(true);
+            try {
+                await onConfirm();
+            } catch (e) {
+                setError(`${e}`);
+            } finally {
+                setInProgres(false);
+            }
+        }, [onConfirm]);
+
+        const formId = 'confirmForm';
+
+        return (
+            <form id={formId}>
+                <Dialog fullWidth {...muiDialogV5(modal)}>
+                    <DialogTitle
+                        component={Heading}
+                        iconLeft={
+                            inProgress ? <LoadingSpinner /> : <CheckIcon />
+                        }
+                    >
+                        {title ?? 'Confirmation'}
+                    </DialogTitle>
+
+                    <DialogContent>
+                        <Stack spacing={2}>
+                            <Typography variant={'body1'}>
+                                {description ?? 'Are you sure?'}
+                            </Typography>
+                            {error != '' && (
+                                <Typography
+                                    variant={'body1'}
+                                    sx={{ color: theme.palette.error.main }}
+                                >
+                                    {error}
+                                </Typography>
+                            )}
+                        </Stack>
+                    </DialogContent>
+                    <DialogActions>
+                        <CancelButton onClick={modal.hide} />
+                        <ConfirmButton
+                            onClick={onSubmit}
+                            disabled={error != ''}
+                        />
+                    </DialogActions>
+                </Dialog>
+            </form>
+        );
+    }
+);
--- frontend/src/component/modal/index.ts
+++ frontend/src/component/modal/index.ts
@@ -1,6 +1,9 @@
 import NiceModal from '@ebay/nice-modal-react';
 import { ContestEditor } from './ContestEditor';
+import { Confirm } from './Confirm';
 
 export const ModalContestEditor = 'modal-contest-editor';
+export const ModalConfirm = 'modal-confirm';
 
 NiceModal.register(ModalContestEditor, ContestEditor);
+NiceModal.register(ModalConfirm, Confirm);
