cbd8932f0eb748fa9d1f88397b44394482f72ac1
Leigh MacDonald <leigh.macdonald@gmail.com>
2023-10-30 04:41:59 -0600

    Add missing operation from downward migration

--- internal/store/migrations/000070_add_members_table.down.sql
+++ internal/store/migrations/000070_add_members_table.down.sql
@@ -5,4 +5,6 @@ drop table if exists members;
 ALTER TABLE IF EXISTS ban
     DROP COLUMN IF EXISTS include_friends;
 
+CREATE UNIQUE INDEX ban_group_group_id_uindex ON gbans.public.ban_group (group_id);
+
 COMMIT;
