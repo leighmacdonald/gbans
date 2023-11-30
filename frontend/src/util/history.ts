87ba4c568dbb189681810b03b59b918f204dd1d0
Leigh MacDonald <leigh.macdonald@gmail.com>
2023-11-30 08:15:06 -0700

    Support deep link to message.

--- frontend/src/util/history.ts
+++ frontend/src/util/history.ts
@@ -1,3 +1,5 @@
+import { useEffect, useRef } from 'react';
+import { useLocation } from 'react-router';
 import { createBrowserHistory } from 'history';
 import SteamID from 'steamid';
 
@@ -11,6 +13,32 @@ export interface LinkProps {
     title: string;
     url: string;
 }
+
+export const useScrollToLocation = () => {
+    const scrolledRef = useRef(false);
+    const { hash } = useLocation();
+    const hashRef = useRef(hash);
+
+    useEffect(() => {
+        if (hash) {
+            // We want to reset if the hash has changed
+            if (hashRef.current !== hash) {
+                hashRef.current = hash;
+                scrolledRef.current = false;
+            }
+
+            // only attempt to scroll if we haven't yet (this could have just reset above if hash changed)
+            if (!scrolledRef.current) {
+                const id = hash.replace('#', '');
+                const element = document.getElementById(id);
+                if (element) {
+                    element.scrollIntoView({ behavior: 'smooth' });
+                    scrolledRef.current = true;
+                }
+            }
+        }
+    });
+};
 
 export const createExternalLinks = (steam_id_str: string): LinkProps[] => {
     const steam_id = new SteamID(steam_id_str);
