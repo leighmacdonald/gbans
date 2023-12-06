1cbec0eca4d2e2c8996367a1ae6c551034092f0d
Leigh MacDonald <leigh.macdonald@gmail.com>
2023-12-06 06:50:14 -0700

    Update binary path

--- pkg/demoparser/demo_parser_test.go
+++ pkg/demoparser/demo_parser_test.go
@@ -10,15 +10,21 @@ import (
 )
 
 func TestParse(t *testing.T) {
-	path, _ := filepath.Abs("../../testdata/test.dem")
+	path, _ := filepath.Abs("testdata/test.dem")
+	if !demoparser.Exists(path) {
+		path, _ = filepath.Abs("../../testdata/test.dem")
+		if !demoparser.Exists(path) {
+			return
+		}
+	}
 
 	var info demoparser.DemoInfo
 
 	require.NoError(t, demoparser.Parse(context.Background(), path, &info))
-	require.Equal(t, 0, len(info.Chat))
-	require.Equal(t, 0, len(info.Deaths))
-	require.Equal(t, 0, len(info.Rounds))
-	require.Equal(t, 4, len(info.Users))
-	require.Equal(t, 25827, info.StartTick)
+	require.Equal(t, 20, len(info.Chat))
+	require.Equal(t, 243, len(info.Deaths))
+	require.Equal(t, 2, len(info.Rounds))
+	require.Equal(t, 45, len(info.Users))
+	require.Equal(t, 509, info.StartTick)
 	require.Equal(t, 0.015, info.IntervalPerTick)
 }
