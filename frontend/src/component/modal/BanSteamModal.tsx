a3055dd8a28b3f675278814c66ebccad657df37a
Leigh MacDonald <leigh.macdonald@gmail.com>
2023-12-08 04:58:43 -0700

    Missing dep

--- frontend/src/component/modal/BanSteamModal.tsx
+++ frontend/src/component/modal/BanSteamModal.tsx
@@ -133,7 +133,7 @@ export const BanSteamModal = NiceModal.create(
                     }
                 }
             },
-            [existing, isUpdate, modal]
+            [existing, isUpdate, modal, sendFlash]
         );
 
         return (
