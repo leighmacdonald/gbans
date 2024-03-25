466aca965b7c1ffd9df060537fb6f7f8569bbb8d
Leigh MacDonald <leigh.macdonald@gmail.com>
2024-03-25 01:15:23 -0600

    Update test

--- internal/wiki/wiki_usecase_test.go
+++ internal/wiki/wiki_usecase_test.go
@@ -12,33 +12,34 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestGetWikiPageBySlug(t *testing.T) {
+func TestGetWikiPageBySlugSuccess(t *testing.T) {
 	user := domain.UserProfile{
 		PermissionLevel: domain.PGuest,
 	}
 
-	t.Run("success", func(t *testing.T) {
-		mockPage := domain.NewWikiPage("test", util.SecureRandomString(100))
-		mockRepo := new(mocks.MockWikiRepository)
-		mockRepo.
-			On("GetWikiPageBySlug", mock.Anything, mock.AnythingOfType("string")).
-			Return(mockPage, nil).
-			Once()
+	mockPage := domain.NewWikiPage("test", util.SecureRandomString(100))
+	mockRepo := new(mocks.MockWikiRepository)
+	mockRepo.
+		On("GetWikiPageBySlug", mock.Anything, mock.AnythingOfType("string")).
+		Return(mockPage, nil)
 
-		u := wiki.NewWikiUsecase(mockRepo)
-		page, errPage := u.GetWikiPageBySlug(context.TODO(), user, "test")
-		require.NoError(t, errPage)
-		require.EqualValues(t, page, mockPage)
-	})
+	u := wiki.NewWikiUsecase(mockRepo)
+	page, errPage := u.GetWikiPageBySlug(context.TODO(), user, "test")
+	require.NoError(t, errPage)
+	require.EqualValues(t, page, mockPage)
+}
 
-	t.Run("error", func(t *testing.T) {
-		mockRepo := new(mocks.MockWikiRepository)
-		mockRepo.
-			On("GetWikiPageBySlug", mock.Anything, mock.AnythingOfType("string")).
-			Return(domain.WikiPage{}, domain.ErrNoResult).
-			Once()
+func TestGetWikiPageBySlugMissing(t *testing.T) {
+	user := domain.UserProfile{
+		PermissionLevel: domain.PGuest,
+	}
 
-		_, errPage := wiki.NewWikiUsecase(mockRepo).GetWikiPageBySlug(context.TODO(), user, "invalid_slug")
-		require.Error(t, errPage)
-	})
+	mockRepo := new(mocks.MockWikiRepository)
+	mockRepo.
+		On("GetWikiPageBySlug", mock.Anything, mock.AnythingOfType("string")).
+		Return(domain.WikiPage{}, domain.ErrNoResult).
+		Once()
+
+	_, errPage := wiki.NewWikiUsecase(mockRepo).GetWikiPageBySlug(context.TODO(), user, "invalid_slug")
+	require.Error(t, errPage)
 }
