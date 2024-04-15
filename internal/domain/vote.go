3899c5e91faf58c18a4c9739a5a9b0d8e009c9dd
Leigh MacDonald <leigh.macdonald@gmail.com>
2024-04-15 15:56:02 -0600

    Add vote result table and initial query.

--- internal/domain/vote.go
+++ internal/domain/vote.go
@@ -4,21 +4,34 @@ import (
 	"context"
 	"github.com/gofrs/uuid/v5"
 	"github.com/leighmacdonald/steamid/v4/steamid"
+	"time"
 )
 
+type VoteQueryFilter struct {
+	QueryFilter
+	SourceID steamid.SteamID
+	TargetID steamid.SteamID
+	ServerID int
+	MatchID  uuid.UUID
+	Name     string
+}
+
 type VoteRepository interface {
+	Query(ctx context.Context, filter VoteQueryFilter) ([]VoteResult, error)
+	AddResult(ctx context.Context, voteResult VoteResult) error
 }
 
 type VoteUsecase interface {
+	Query(ctx context.Context, filter VoteQueryFilter) ([]VoteResult, error)
 	Start(ctx context.Context)
 }
 
 type VoteResult struct {
-	ServerID int
-	MatchID  uuid.UUID
-	SourceID steamid.SteamID
-	TargetID steamid.SteamID
-	Valid    int
-	Name     string
-	Proxy    int
+	ServerID  int
+	MatchID   uuid.UUID
+	SourceID  steamid.SteamID
+	TargetID  steamid.SteamID
+	Valid     int
+	Name      string
+	CreatedOn time.Time
 }
