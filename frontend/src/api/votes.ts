b24e2138e97ea210764ffd38a5f214fd4d779f7b
Leigh MacDonald <leigh.macdonald@gmail.com>
2024-04-15 22:45:49 -0600

    Add initial votes frontend.

---
+++ frontend/src/api/votes.ts
@@ -0,0 +1,38 @@
+import { LazyResult } from '../util/table.ts';
+import { apiCall, QueryFilter, transformCreatedOnDate } from './common.ts';
+
+export type VoteResult = {
+    server_id: number;
+    match_id: string;
+    source_id: string;
+    source_name: string;
+    source_avatar_hash: string;
+    target_id: string;
+    target_name: string;
+    target_avatar_hash: string;
+    valid: boolean;
+    success: boolean;
+    code: number;
+    created_on: Date;
+};
+
+export type VoteQueryFilter = {
+    source_id: string;
+    target_id: string;
+    success: boolean;
+} & QueryFilter<VoteResult>;
+
+export const apiVotesQuery = async (
+    opts: VoteQueryFilter,
+    abortController?: AbortController
+) => {
+    const resp = await apiCall<LazyResult<VoteResult>>(
+        '/api/votes',
+        'POST',
+        opts,
+        abortController
+    );
+    resp.data = resp.data.map(transformCreatedOnDate);
+
+    return resp;
+};
