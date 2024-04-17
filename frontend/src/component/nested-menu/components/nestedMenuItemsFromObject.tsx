177e852f060dee7887141c3122cfd4fd8904bd3e
Leigh MacDonald <leigh.macdonald@gmail.com>
2024-04-17 17:16:25 -0600

    Remove unnecessary import

--- frontend/src/component/nested-menu/components/nestedMenuItemsFromObject.tsx
+++ frontend/src/component/nested-menu/components/nestedMenuItemsFromObject.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
 import { MenuItemData } from '../definitions';
 import { IconMenuItem } from './IconMenuItem';
