427a161801e8261a5d74d2ffaa697f165d01c800
Leigh MacDonald <leigh.macdonald@gmail.com>
2024-05-02 01:32:26 -0600

    Migrate asn ban table

--- frontend/src/routes/_mod.admin.ban.asn.tsx
+++ frontend/src/routes/_mod.admin.ban.asn.tsx
@@ -1,11 +1,20 @@
 import AddIcon from '@mui/icons-material/Add';
 import GavelIcon from '@mui/icons-material/Gavel';
 import Button from '@mui/material/Button';
+import Link from '@mui/material/Link';
+import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Unstable_Grid2';
-import { createFileRoute } from '@tanstack/react-router';
+import { useQuery } from '@tanstack/react-query';
+import { createFileRoute, Link as RouterLink } from '@tanstack/react-router';
+import { createColumnHelper, getCoreRowModel, useReactTable } from '@tanstack/react-table';
 import { z } from 'zod';
+import { apiGetBansASN, ASNBanRecord, BanReasons } from '../api';
 import { ContainerWithHeaderAndButtons } from '../component/ContainerWithHeaderAndButtons.tsx';
-import { commonTableSearchSchema } from '../util/table.ts';
+import { DataTable, HeadingCell } from '../component/DataTable.tsx';
+import { PersonCell } from '../component/PersonCell.tsx';
+import { TableCellRelativeDateField } from '../component/table/TableCellRelativeDateField.tsx';
+import { commonTableSearchSchema, isPermanentBan, LazyResult } from '../util/table.ts';
+import { renderDate } from '../util/text.tsx';
 
 const banASNSearchSchema = z.object({
     ...commonTableSearchSchema,
@@ -22,6 +31,20 @@ export const Route = createFileRoute('/_mod/admin/ban/asn')({
 });
 
 function AdminBanASN() {
+    const { page, rows, sortOrder, sortColumn, target_id, source_id } = Route.useSearch();
+    const { data: bans, isLoading } = useQuery({
+        queryKey: ['steamBans', { page, rows, sortOrder, sortColumn, target_id, source_id }],
+        queryFn: async () => {
+            return await apiGetBansASN({
+                limit: Number(rows),
+                offset: Number((page ?? 0) * rows),
+                order_by: sortColumn ?? 'ban_asn_id',
+                desc: sortOrder == 'desc',
+                source_id: source_id,
+                target_id: target_id
+            });
+        }
+    });
     // const [newASNBans, setNewASNBans] = useState<ASNBanRecord[]>([]);
 
     // const onNewBanASN = useCallback(async () => {
@@ -295,8 +318,78 @@ function AdminBanASN() {
                     {/*        }*/}
                     {/*    ]}*/}
                     {/*/>*/}
+                    <BanASMTable bans={bans ?? { data: [], count: 0 }} isLoading={isLoading} />
                 </ContainerWithHeaderAndButtons>
             </Grid>
         </Grid>
     );
 }
+
+const columnHelper = createColumnHelper<ASNBanRecord>();
+
+const BanASMTable = ({ bans, isLoading }: { bans: LazyResult<ASNBanRecord>; isLoading: boolean }) => {
+    const columns = [
+        columnHelper.accessor('ban_asn_id', {
+            header: () => <HeadingCell name={'Ban ID'} />,
+            cell: (info) => (
+                <Link component={RouterLink} to={`/ban/$ban_id`} params={{ ban_id: info.getValue() }}>
+                    {`#${info.getValue()}`}
+                </Link>
+            )
+        }),
+        columnHelper.accessor('source_id', {
+            header: () => <HeadingCell name={'Author'} />,
+            cell: (info) => (
+                <PersonCell
+                    steam_id={bans.data[info.row.index].source_id}
+                    personaname={bans.data[info.row.index].source_personaname}
+                    avatar_hash={bans.data[info.row.index].source_avatarhash}
+                />
+            )
+        }),
+        columnHelper.accessor('target_id', {
+            header: () => <HeadingCell name={'Subject'} />,
+            cell: (info) => (
+                <PersonCell
+                    steam_id={bans.data[info.row.index].target_id}
+                    personaname={bans.data[info.row.index].target_personaname}
+                    avatar_hash={bans.data[info.row.index].target_avatarhash}
+                />
+            )
+        }),
+        columnHelper.accessor('as_num', {
+            header: () => <HeadingCell name={'ASN'} />,
+            cell: (info) => <Typography>{info.getValue()}</Typography>
+        }),
+        columnHelper.accessor('reason', {
+            header: () => <HeadingCell name={'Reason'} />,
+            cell: (info) => <Typography>{BanReasons[info.getValue()]}</Typography>
+        }),
+        columnHelper.accessor('created_on', {
+            header: () => <HeadingCell name={'Created'} />,
+            cell: (info) => <Typography>{renderDate(info.getValue())}</Typography>
+        }),
+        columnHelper.accessor('valid_until', {
+            header: () => <HeadingCell name={'Expires'} />,
+            cell: (info) =>
+                isPermanentBan(bans.data[info.row.index].created_on, bans.data[info.row.index].valid_until) ? (
+                    'Permanent'
+                ) : (
+                    <TableCellRelativeDateField
+                        date={bans.data[info.row.index].created_on}
+                        compareDate={bans.data[info.row.index].valid_until}
+                    />
+                )
+        })
+    ];
+
+    const table = useReactTable({
+        data: bans.data,
+        columns: columns,
+        getCoreRowModel: getCoreRowModel(),
+        manualPagination: true,
+        autoResetPageIndex: true
+    });
+
+    return <DataTable table={table} isLoading={isLoading} />;
+};
