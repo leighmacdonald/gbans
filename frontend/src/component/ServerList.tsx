714c0fc89ed237a3bec81768de1d8f9b8638c10e
Leigh MacDonald <leigh.macdonald@gmail.com>
2024-05-06 04:25:44 -0600

    Migrate servers table. Remove LazyTable

--- frontend/src/component/ServerList.tsx
+++ frontend/src/component/ServerList.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from 'react';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 import StorageIcon from '@mui/icons-material/Storage';
@@ -6,20 +7,107 @@ import IconButton from '@mui/material/IconButton';
 import Link from '@mui/material/Link';
 import Tooltip from '@mui/material/Tooltip';
 import Typography from '@mui/material/Typography';
+import { createColumnHelper, getCoreRowModel, TableOptions, useReactTable } from '@tanstack/react-table';
 import { BaseServer, cleanMapName } from '../api';
 import { useMapStateCtx } from '../hooks/useMapStateCtx.ts';
 import { useUserFlashCtx } from '../hooks/useUserFlashCtx.ts';
 import { tf2Fonts } from '../theme';
 import { logErr } from '../util/errors';
-import { RowsPerPage } from '../util/table.ts';
 import { ContainerWithHeader } from './ContainerWithHeader';
+import { DataTable } from './DataTable.tsx';
 import { Flag } from './Flag';
 import { LoadingSpinner } from './LoadingSpinner';
-import { LazyTable } from './table/LazyTable';
+import { TableHeadingCell } from './TableHeadingCell.tsx';
+
+type ServerRow = BaseServer & { copy: string; connect: string };
 
 export const ServerList = () => {
     const { sendFlash } = useUserFlashCtx();
     const { selectedServers } = useMapStateCtx();
+
+    const columnHelper = createColumnHelper<ServerRow>();
+
+    const columns = [
+        columnHelper.accessor('cc', {
+            header: () => <TableHeadingCell name={'CC'} />,
+            cell: (info) => <Flag countryCode={info.getValue()} />
+        }),
+        columnHelper.accessor('name', {
+            header: () => <TableHeadingCell name={'Server'} />,
+            cell: (info) => (
+                <Typography variant={'button'} fontFamily={tf2Fonts}>
+                    {info.getValue()}
+                </Typography>
+            )
+        }),
+        columnHelper.accessor('map', {
+            header: () => <TableHeadingCell name={'Map'} />,
+            cell: (info) => <Typography variant={'body2'}>{cleanMapName(info.getValue())}</Typography>
+        }),
+        columnHelper.accessor('players', {
+            header: () => <TableHeadingCell name={'Players'} />,
+            cell: (info) => <Typography variant={'body2'}>{`${info.getValue()}/${selectedServers[info.row.index].max_players}`}</Typography>
+        }),
+        columnHelper.accessor('distance', {
+            header: () => <TableHeadingCell name={'Dist'} />,
+            cell: (info) => (
+                <Tooltip title={`Distance in hammer units: ${Math.round((info.getValue() ?? 1) * 52.49)} khu`}>
+                    <Typography variant={'caption'}>{`${info.getValue().toFixed(0)}km`}</Typography>
+                </Tooltip>
+            )
+        }),
+        columnHelper.accessor('copy', {
+            header: () => <TableHeadingCell name={'Cp'} />,
+            cell: (info) => (
+                <IconButton
+                    color={'primary'}
+                    aria-label={'Copy connect string to clipboard'}
+                    onClick={() => {
+                        navigator.clipboard
+                            .writeText(`connect ${selectedServers[info.row.index].host}:${selectedServers[info.row.index].port}`)
+                            .then(() => {
+                                sendFlash('success', 'Copied address to clipboard');
+                            })
+                            .catch((e) => {
+                                sendFlash('error', 'Failed to copy address');
+                                logErr(e);
+                            });
+                    }}
+                >
+                    <ContentCopyIcon />
+                </IconButton>
+            )
+        }),
+        columnHelper.accessor('connect', {
+            header: () => <TableHeadingCell name={'Connect'} />,
+            cell: (info) => (
+                <Button
+                    fullWidth
+                    endIcon={<ChevronRightIcon />}
+                    component={Link}
+                    href={`steam://connect/${selectedServers[info.row.index].ip}:${selectedServers[info.row.index].port}`}
+                    variant={'contained'}
+                    sx={{ minWidth: 100 }}
+                >
+                    Join
+                </Button>
+            )
+        })
+    ];
+
+    const metaServers = useMemo(() => {
+        return selectedServers.map((s) => ({ ...s, copy: '', connect: '' }));
+    }, [selectedServers]);
+
+    const opts: TableOptions<ServerRow> = {
+        data: metaServers,
+        columns: columns,
+        getCoreRowModel: getCoreRowModel(),
+        manualPagination: false,
+        autoResetPageIndex: true
+    };
+
+    const table = useReactTable(opts);
 
     if (selectedServers.length === 0) {
         return (
@@ -29,119 +117,5 @@ export const ServerList = () => {
         );
     }
 
-    return (
-        <ContainerWithHeader title={'Servers'} iconLeft={<StorageIcon />}>
-            <LazyTable<BaseServer>
-                hideHeader={true}
-                rows={selectedServers}
-                sortOrder={'asc'}
-                onSortColumnChanged={() => {}}
-                onSortOrderChanged={() => {}}
-                sortColumn={'distance'}
-                rowsPerPage={RowsPerPage.Hundred}
-                columns={[
-                    {
-                        label: 'CC',
-                        tooltip: 'Country Code',
-                        sortKey: 'cc',
-                        sortType: 'string',
-                        sortable: false,
-                        renderer: (_, value) => <Flag countryCode={value as string} />
-                    },
-                    {
-                        label: 'Server',
-                        tooltip: 'Server Name',
-                        sortKey: 'name',
-                        sortType: 'string',
-                        sortable: false,
-                        align: 'left',
-                        width: '100%',
-                        renderer: (_, value) => (
-                            <Typography variant={'button'} fontFamily={tf2Fonts}>
-                                {value as string}
-                            </Typography>
-                        )
-                    },
-                    {
-                        label: 'Map',
-                        tooltip: 'Map Name',
-                        sortKey: 'map',
-                        sortType: 'string',
-                        sortable: false,
-                        renderer: (obj) => {
-                            return <Typography variant={'body2'}>{cleanMapName(obj.map)}</Typography>;
-                        }
-                    },
-                    {
-                        label: 'Players',
-                        tooltip: 'Current Players',
-                        sortKey: 'players',
-                        sortable: false,
-                        renderer: (obj, value) => {
-                            return <Typography variant={'body2'}>{`${value}/${obj.max_players}`}</Typography>;
-                        }
-                    },
-                    {
-                        label: 'Dist',
-                        tooltip: `Distance to the server`,
-                        sortKey: 'distance',
-                        sortable: false,
-                        renderer: (obj) => {
-                            return (
-                                <Tooltip title={`Distance in hammer units: ${Math.round((obj.distance ?? 1) * 52.49)} khu`}>
-                                    <Typography variant={'caption'}>{`${obj.distance.toFixed(0)}km`}</Typography>
-                                </Tooltip>
-                            );
-                        }
-                    },
-                    {
-                        label: 'Cp',
-                        virtual: true,
-                        virtualKey: 'copy',
-                        tooltip: 'Copy server address to clipboard',
-                        renderer: (obj) => {
-                            return (
-                                <IconButton
-                                    color={'primary'}
-                                    aria-label={'Copy connect string to clipboard'}
-                                    onClick={() => {
-                                        navigator.clipboard
-                                            .writeText(`connect ${obj.host}:${obj.port}`)
-                                            .then(() => {
-                                                sendFlash('success', 'Copied address to clipboard');
-                                            })
-                                            .catch((e) => {
-                                                sendFlash('error', 'Failed to copy address');
-                                                logErr(e);
-                                            });
-                                    }}
-                                >
-                                    <ContentCopyIcon />
-                                </IconButton>
-                            );
-                        }
-                    },
-                    {
-                        label: 'Connect',
-                        virtual: true,
-                        virtualKey: 'connect',
-                        tooltip: 'Connect to a server',
-                        renderer: (serverState) => {
-                            return (
-                                <Button
-                                    endIcon={<ChevronRightIcon />}
-                                    component={Link}
-                                    href={`steam://connect/${serverState.ip}:${serverState.port}`}
-                                    variant={'contained'}
-                                    sx={{ minWidth: 100 }}
-                                >
-                                    Join
-                                </Button>
-                            );
-                        }
-                    }
-                ]}
-            />
-        </ContainerWithHeader>
-    );
+    return <DataTable table={table} isLoading={false} />;
 };
