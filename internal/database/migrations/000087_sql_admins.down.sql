62c0576f04ddb5e522163231e68a26e018a6e6f9
Leigh MacDonald <leigh.macdonald@gmail.com>
2024-05-12 21:13:11 -0600

    Add initial sql schemas

---
+++ internal/database/migrations/000087_sql_admins.down.sql
@@ -0,0 +1,11 @@
+BEGIN;
+
+DROP TABLE sm_config;
+DROP TABLE sm_admins_groups;
+DROP TABLE sm_overrides;
+DROP TABLE sm_group_overrides;
+DROP TABLE sm_group_immunity;
+DROP TABLE sm_groups;
+DROP TABLE sm_admins;
+
+COMMIT;
