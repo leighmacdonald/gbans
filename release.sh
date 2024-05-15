351bbda0a9d4d7df30ca46d549de9e01074ba93d
Leigh MacDonald <leigh.macdonald@gmail.com>
2024-05-14 20:02:27 -0600

    Add dispatch release workflow

--- release.sh
+++ release.sh
@@ -17,21 +17,8 @@ if [ -z "$CURRENT_VERSION" ]; then
   exit 1
 fi
 
-make fmt
-make check
-make docker_test
-
 echo "Setting version from $CURRENT_VERSION -> $NEXT_VERSION"
 
 sed -i "s/"${CURRENT_VERSION}"/"${NEXT_VERSION}"/g" sourcemod/scripting/gbans/globals.sp
 sed -i "s/"${CURRENT_VERSION}"/"${NEXT_VERSION}"/g" frontend/package.json
-sed -i "s/"v${CURRENT_VERSION}"/v"${NEXT_VERSION}"/g" Dockerfile
 sed -i "s/v${CURRENT_VERSION}/v${NEXT_VERSION}/g" Makefile
-
-git commit -a -m "[Release] Bump version to ${NEXT_VERSION}"
-git push
-
-git tag -a "v${NEXT_VERSION}" -m "v${NEXT_VERSION} Release"
-git push --tags
-
-goreleaser release --clean
