08e24a89c161208e828f6973ee9c95cc97639fcd
Leigh MacDonald <leigh.macdonald@gmail.com>
2025-01-01 16:36:28 -0700

    Update down migration for map

--- internal/database/migrations/000102_create_speedruns_table.down.sql
+++ internal/database/migrations/000102_create_speedruns_table.down.sql
@@ -4,6 +4,7 @@ DROP TABLE IF EXISTS speedrun_rounds_runners;
 DROP TABLE IF EXISTS speedrun_round;
 DROP TABLE IF EXISTS speedrun_runners;
 DROP TABLE IF EXISTS speedrun;
+DROP TABLE IF EXISTS map;
 
 ALTER TABLE config
     DROP COLUMN IF EXISTS general_speedruns_enabled;
