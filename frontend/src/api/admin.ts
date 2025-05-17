fccb4d2c6f5ac8baf65595f39fbffd1b9dbd10bf
Leigh MacDonald <leigh.macdonald@gmail.com>
2025-05-17 02:36:04 -0600

    Move config schemas to own module

--- frontend/src/api/admin.ts
+++ frontend/src/api/admin.ts
@@ -1,3 +1,20 @@
+import { z } from 'zod';
+import {
+    schemaAnticheat,
+    schemaDebug,
+    schemaDemos,
+    schemaDiscord,
+    schemaExports,
+    schemaFilters,
+    schemaGeneral,
+    schemaGeo,
+    schemaLocalStore,
+    schemaLogging,
+    schemaNetwork,
+    schemaPatreon,
+    schemaSentry,
+    schemaSSH
+} from '../schema/config.ts';
 import { apiCall } from './common.ts';
 
 export const apiSaveSettings = async (settings: Config) => {
@@ -9,28 +26,20 @@ export const apiGetSettings = async () => {
 };
 
 export type Config = {
-    general: General;
-    filters: Filters;
-    demo: Demos;
-    patreon: Patreon;
-    discord: Discord;
-    network: Network;
-    log: Logging;
-    sentry: Sentry;
-    geo_location: GeoLocation;
-    debug: Debug;
-    local_store: LocalStore;
-    ssh: SSH;
-    exports: Exports;
-    anticheat: Anticheat;
-};
-
-type Network = {
-    sdr_enabled: boolean;
-    sdr_dns_enabled: boolean;
-    cf_key: string;
-    cf_email: string;
-    cf_zone_id: string;
+    general: z.infer<typeof schemaGeneral>;
+    filters: z.infer<typeof schemaFilters>;
+    demo: z.infer<typeof schemaDemos>;
+    patreon: z.infer<typeof schemaPatreon>;
+    discord: z.infer<typeof schemaDiscord>;
+    network: z.infer<typeof schemaNetwork>;
+    log: z.infer<typeof schemaLogging>;
+    sentry: z.infer<typeof schemaSentry>;
+    geo_location: z.infer<typeof schemaGeo>;
+    debug: z.infer<typeof schemaDebug>;
+    local_store: z.infer<typeof schemaLocalStore>;
+    ssh: z.infer<typeof schemaSSH>;
+    exports: z.infer<typeof schemaExports>;
+    anticheat: z.infer<typeof schemaAnticheat>;
 };
 
 export enum Action {
@@ -40,141 +49,3 @@ export enum Action {
 }
 
 export const ActionColl = [Action.Ban, Action.Kick, Action.Gag];
-
-type Anticheat = {
-    enabled: boolean;
-    action: Action;
-    duration: number;
-    max_aim_snap: number;
-    max_psilent: number;
-    max_bhop: number;
-    max_fake_ang: number;
-    max_cmd_num: number;
-    max_too_many_connections: number;
-    max_cheat_cvar: number;
-    max_oob_var: number;
-    max_invalid_user_cmd: number;
-};
-
-type General = {
-    site_name: string;
-    mode: 'release' | 'debug' | 'test';
-    file_serve_mode: 'local';
-    srcds_log_addr: string;
-    asset_url: string;
-    default_route: string;
-    news_enabled: boolean;
-    forums_enabled: boolean;
-    contests_enabled: boolean;
-    wiki_enabled: boolean;
-    stats_enabled: boolean;
-    servers_enabled: boolean;
-    reports_enabled: boolean;
-    chatlogs_enabled: boolean;
-    demos_enabled: boolean;
-    speedruns_enabled: boolean;
-    playerqueue_enabled: boolean;
-};
-
-type Filters = {
-    enabled: boolean;
-    dry: boolean;
-    ping_discord: boolean;
-    max_weight: string;
-    warning_timeout: string;
-    warning_limit: string;
-    check_timeout: string;
-    match_timeout: string;
-};
-
-type Demos = {
-    demo_cleanup_enabled: boolean;
-    demo_cleanup_strategy: 'pctfree' | 'count';
-    demo_cleanup_min_pct: string;
-    demo_cleanup_mount: string;
-    demo_count_limit: string;
-    demo_parser_url: string;
-};
-
-type Patreon = {
-    enabled: boolean;
-    integrations_enabled: boolean;
-    client_id: string;
-    client_secret: string;
-    creator_access_token: string;
-    creator_refresh_token: string;
-};
-
-type Discord = {
-    enabled: boolean;
-    bot_enabled: boolean;
-    integrations_enabled: boolean;
-    app_id: string;
-    app_secret: string;
-    link_id: string;
-    token: string;
-    guild_id: string;
-    anticheat_channel_id: string;
-    log_channel_id: string;
-    public_log_channel_enable: boolean;
-    public_log_channel_id: string;
-    public_match_log_channel_id: string;
-    mod_ping_role_id: string;
-    vote_log_channel_id: string;
-    appeal_log_channel_id: string;
-    ban_log_channel_id: string;
-    forum_log_channel_id: string;
-    kick_log_channel_id: string;
-    word_filter_log_channel_id: string;
-    playerqueue_channel_id: string;
-};
-
-type LogLevels = 'debug' | 'info' | 'warn' | 'error';
-
-type Logging = {
-    level: LogLevels;
-    file: string;
-    http_enabled: boolean;
-    http_otel_enabled: boolean;
-    http_level: LogLevels;
-};
-
-type Sentry = {
-    sentry_dsn: string;
-    sentry_dsn_web: string;
-    sentry_trace: boolean;
-    sentry_sample_rate: string;
-};
-
-type GeoLocation = {
-    enabled: boolean;
-    cache_path: string;
-    token: string;
-};
-
-type Debug = {
-    skip_open_id_validation: boolean;
-    add_rcon_log_address: string;
-};
-
-type LocalStore = {
-    path_root: string;
-};
-
-type SSH = {
-    enabled: boolean;
-    username: string;
-    port: string;
-    private_key_path: string;
-    password: string;
-    update_interval: string;
-    timeout: string;
-    demo_path_fmt: string;
-    stac_path_fmt: string;
-};
-
-type Exports = {
-    bd_enabled: boolean;
-    valve_enabled: boolean;
-    authorized_keys: string;
-};
---
+++ frontend/src/schema/config.ts
@@ -0,0 +1,145 @@
+import { z } from 'zod';
+import { Action } from '../api/admin.ts';
+
+export const schemaAnticheat = z.object({
+    enabled: z.boolean(),
+    action: z.nativeEnum(Action),
+    duration: z.number(),
+    max_aim_snap: z.number(),
+    max_psilent: z.number(),
+    max_bhop: z.number(),
+    max_fake_ang: z.number(),
+    max_cmd_num: z.number(),
+    max_too_many_connections: z.number(),
+    max_cheat_cvar: z.number(),
+    max_oob_var: z.number(),
+    max_invalid_user_cmd: z.number()
+});
+export const schemaSentry = z.object({
+    sentry_dsn: z.string(),
+    sentry_dsn_web: z.string(),
+    sentry_trace: z.boolean(),
+    sentry_sample_rate: z.number()
+});
+
+export const schemaGeneral = z.object({
+    site_name: z.string().min(1).max(32),
+    file_serve_mode: z.enum(['local']),
+    mode: z.enum(['release', 'debug', 'test']),
+    srcds_log_addr: z.string(),
+    default_route: z.string(),
+    asset_url: z.string().url(),
+    contests_enabled: z.boolean(),
+    news_enabled: z.boolean(),
+    forums_enabled: z.boolean(),
+    wiki_enabled: z.boolean(),
+    stats_enabled: z.boolean(),
+    servers_enabled: z.boolean(),
+    reports_enabled: z.boolean(),
+    chatlogs_enabled: z.boolean(),
+    demos_enabled: z.boolean(),
+    speedruns_enabled: z.boolean(),
+    playerqueue_enabled: z.boolean()
+});
+
+export const schemaFilters = z.object({
+    enabled: z.boolean(),
+    warning_timeout: z.number().min(1).max(1000000),
+    warning_limit: z.number().min(0).max(1000),
+    dry: z.boolean(),
+    ping_discord: z.boolean(),
+    max_weight: z.number().min(1).max(1000),
+    check_timeout: z.number().min(5).max(300),
+    match_timeout: z.number().min(1).max(1000)
+});
+
+export const schemaDemos = z.object({
+    demo_cleanup_enabled: z.boolean(),
+    demo_cleanup_strategy: z.enum(['pctfree', 'count']),
+    demo_cleanup_min_pct: z.number().min(0).max(100),
+    demo_cleanup_mount: z.string().startsWith('/'), // windows?
+    demo_count_limit: z.number(),
+    demo_parser_url: z.string().url()
+});
+
+export const schemaPatreon = z.object({
+    enabled: z.boolean(),
+    integrations_enabled: z.boolean(),
+    client_id: z.string(),
+    client_secret: z.string(),
+    creator_access_token: z.string(),
+    creator_refresh_token: z.string()
+});
+
+export const schemaDiscord = z.object({
+    enabled: z.boolean(),
+    bot_enabled: z.boolean(),
+    integrations_enabled: z.boolean(),
+    app_id: z.string().refine((arg) => arg.length == 0 || arg.length == 18),
+    app_secret: z.string(),
+    link_id: z.string(),
+    token: z.string(),
+    guild_id: z.string(),
+    log_channel_id: z.string(),
+    public_log_channel_enable: z.boolean(),
+    public_log_channel_id: z.string(),
+    public_match_log_channel_id: z.string(),
+    mod_ping_role_id: z.string(),
+    vote_log_channel_id: z.string(),
+    appeal_log_channel_id: z.string(),
+    ban_log_channel_id: z.string(),
+    forum_log_channel_id: z.string(),
+    word_filter_log_channel_id: z.string(),
+    kick_log_channel_id: z.string(),
+    playerqueue_channel_id: z.string(),
+    anticheat_channel_id: z.string()
+});
+
+export const schemaLogging = z.object({
+    level: z.enum(['debug', 'info', 'warn', 'error']),
+    file: z.string(),
+    http_enabled: z.boolean(),
+    http_otel_enabled: z.boolean(),
+    http_level: z.enum(['debug', 'info', 'warn', 'error'])
+});
+
+export const schemaGeo = z.object({
+    enabled: z.boolean(),
+    cache_path: z.string(),
+    token: z.string().refine((arg) => arg.length == 0 || arg.length == 64)
+});
+
+export const schemaDebug = z.object({
+    skip_open_id_validation: z.boolean(),
+    add_rcon_log_address: z.string()
+});
+
+export const schemaLocalStore = z.object({
+    path_root: z.string()
+});
+
+export const schemaSSH = z.object({
+    enabled: z.boolean(),
+    username: z.string(),
+    port: z.number().min(1).max(65535),
+    private_key_path: z.string(),
+    password: z.string(),
+    update_interval: z.number().positive(),
+    timeout: z.number().positive(),
+    demo_path_fmt: z.string(),
+    stac_path_fmt: z.string()
+});
+
+export const schemaExports = z.object({
+    bd_enabled: z.boolean(),
+    valve_enabled: z.boolean(),
+    authorized_keys: z.string()
+});
+
+export const schemaNetwork = z.object({
+    sdr_enabled: z.boolean(),
+    sdr_dns_enabled: z.boolean(),
+    cf_key: z.string(),
+    cf_email: z.string().email(),
+    cf_zone_id: z.string()
+});
