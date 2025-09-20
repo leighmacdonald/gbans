16b118eed0284cd4ad4730f3831de151f66fdbdf
Leigh MacDonald <leigh.macdonald@gmail.com>
2025-09-20 03:30:05 -0600

    Initial reimplementation of asn blocks.

---
+++ internal/network/asn/asn.go
@@ -0,0 +1,52 @@
+package asn
+
+import (
+	"context"
+	"errors"
+	"time"
+)
+
+var ErrInvalidASNBan = errors.New("invalid asnban")
+
+// Block represents a autonomous systems number based network block.
+type Block struct {
+	ASNum int `json:"as_num"`
+	// Reason is the one liner reason shown to banned users upon connect.
+	Reason string `json:"reason"`
+	// Notes is the hidden moderator/admin notes for the ban.
+	Notes     string    `json:"notes"`
+	CreatedOn time.Time `json:"created_on"`
+	UpdatedOn time.Time `json:"updated_on"`
+}
+
+func NewBlock(asNum int, reason string) Block {
+	return Block{ASNum: asNum, Reason: reason, CreatedOn: time.Now(), UpdatedOn: time.Now()}
+}
+
+func NewBlocker(repo Repository) Blocker {
+	return Blocker{repo: repo}
+}
+
+type Blocker struct {
+	repo Repository
+}
+
+func (a Blocker) Check(ctx context.Context, addr string) error {
+	return nil
+}
+
+func (a Blocker) Save(ctx context.Context, asnBan Block) error {
+	if asnBan.ASNum <= 0 {
+		return ErrInvalidASNBan
+	}
+
+	return a.repo.Save(ctx, asnBan)
+}
+
+func (a Blocker) Delete(ctx context.Context, asnBan Block) error {
+	if asnBan.ASNum <= 0 {
+		return ErrInvalidASNBan
+	}
+
+	return a.repo.Delete(ctx, asnBan.ASNum)
+}
---
+++ internal/network/asn/asn_repo.go
@@ -0,0 +1,54 @@
+package asn
+
+import (
+	"context"
+
+	"github.com/leighmacdonald/gbans/internal/database"
+)
+
+func NewASNRepository(db database.Database) Repository {
+	return Repository{db: db}
+}
+
+type Repository struct {
+	db database.Database
+}
+
+func (r Repository) All(ctx context.Context) ([]Block, error) {
+	rows, errRows := r.db.Query(ctx, nil, `SELECT as_num, reason, notes, created_on, updated_on FROM as_num`)
+	if errRows != nil {
+		return nil, database.DBErr(errRows)
+	}
+
+	defer rows.Close()
+
+	var blocks []Block
+	for rows.Next() {
+		var block Block
+		if err := rows.Scan(&block.ASNum, &block.Reason, &block.Notes, &block.CreatedOn, &block.UpdatedOn); err != nil {
+			return nil, database.DBErr(err)
+		}
+
+		blocks = append(blocks, block)
+	}
+
+	return blocks, nil
+}
+
+func (r Repository) Save(ctx context.Context, ban Block) error {
+	const query = `
+		INSERT INTO asn_ban (as_num, reason, notes, created_on, updated_on)
+		VALUES ($1, $2, $3, $4, $5)
+		ON CONFLICT (as_num) DO UPDATE
+		SET reason = $2, updated_on = $5`
+
+	if err := r.db.Exec(ctx, nil, query, ban.ASNum, ban.Reason, ban.CreatedOn, ban.UpdatedOn); err != nil {
+		return database.DBErr(err)
+	}
+
+	return nil
+}
+
+func (r Repository) Delete(ctx context.Context, asNum int) error {
+	return database.DBErr(r.db.Exec(ctx, nil, `DELETE FROM ban_asn WHERE as_num = $1`, asNum))
+}
