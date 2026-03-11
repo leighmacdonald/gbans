ddd7c68c44f73bea8bf13f7db2ca10b0311a7808
Leigh MacDonald <leigh.macdonald@gmail.com>
2026-03-11 02:27:41 -0600

    Appeals table

--- frontend/src/routes/_mod.admin.appeals.tsx
+++ frontend/src/routes/_mod.admin.appeals.tsx
@@ -5,35 +5,29 @@ import MenuItem from "@mui/material/MenuItem";
 import Typography from "@mui/material/Typography";
 import { useQuery } from "@tanstack/react-query";
 import { createFileRoute, useNavigate } from "@tanstack/react-router";
-import {
-	type ColumnFiltersState,
-	createColumnHelper,
-	getCoreRowModel,
-	getFilteredRowModel,
-	getPaginationRowModel,
-	getSortedRowModel,
-	type SortingState,
-	useReactTable,
-} from "@tanstack/react-table";
-import { useState } from "react";
+import { createMRTColumnHelper, MaterialReactTable, useMaterialReactTable } from "material-react-table";
+import { useMemo } from "react";
 import { z } from "zod/v4";
 import { apiGetAppeals, appealStateString } from "../api";
 import { ContainerWithHeader } from "../component/ContainerWithHeader.tsx";
-import { PaginatorLocal } from "../component/forum/PaginatorLocal.tsx";
 import { PersonCell } from "../component/PersonCell.tsx";
 import { TextLink } from "../component/TextLink.tsx";
-import { DataTable } from "../component/table/DataTable.tsx";
+import { createDefaultTableOptions } from "../component/table/options.ts";
+import { TableCellRelativeDateField } from "../component/table/TableCellRelativeDateField.tsx";
 import { useAppForm } from "../contexts/formContext.tsx";
 import {
 	AppealState,
 	AppealStateCollection,
 	type AppealStateEnum,
+	BanReason,
 	BanReasons,
 	type BanRecord,
 } from "../schema/bans.ts";
-import type { TablePropsAll } from "../types/table.ts";
-import { commonTableSearchSchema, initColumnFilter, initPagination, initSortOrder } from "../util/table.ts";
+import { commonTableSearchSchema } from "../util/table.ts";
 import { renderDateTime } from "../util/time.ts";
+
+const columnHelper = createMRTColumnHelper<BanRecord>();
+const defaultOptions = createDefaultTableOptions<BanRecord>();
 
 const appealSearchSchema = commonTableSearchSchema.extend({
 	sortColumn: z
@@ -56,40 +50,30 @@ export const Route = createFileRoute("/_mod/admin/appeals")({
 	head: ({ match }) => ({
 		meta: [{ name: "description", content: "Appeals" }, match.context.title("Appeals")],
 	}),
+	loader: async () => {
+		const data = await apiGetAppeals({});
+		return { data };
+	},
 });
 
 function AdminAppeals() {
 	const navigate = useNavigate({ from: Route.fullPath });
 	const search = Route.useSearch();
-	const [pagination, setPagination] = useState(initPagination(search.pageIndex, search.pageSize));
-	const [sorting, setSorting] = useState<SortingState>(
-		initSortOrder(search.sortColumn, search.sortOrder, {
-			id: "created_on",
-			desc: true,
-		}),
-	);
+
 	const defaultValues: z.infer<typeof schema> = {
 		source_id: search.source_id ?? "",
 		target_id: search.target_id ?? "",
 		appeal_state: search.appeal_state ?? AppealState.Any,
 	};
-	const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>(initColumnFilter(search));
-	const { data: appeals, isLoading } = useQuery({
+	const { data } = useQuery({
 		queryKey: ["appeals"],
 		queryFn: async () => {
-			return await apiGetAppeals({});
+			return (await apiGetAppeals({})) ?? [];
 		},
 	});
 
 	const form = useAppForm({
 		onSubmit: async ({ value }) => {
-			setColumnFilters(
-				initColumnFilter({
-					appeal_state: value.appeal_state ?? AppealState.Any,
-					source_id: value.source_id ?? undefined,
-					target_id: value.target_id ?? undefined,
-				}),
-			);
 			await navigate({
 				to: "/admin/appeals",
 				search: (prev) => ({ ...prev, ...value }),
@@ -118,7 +102,109 @@ function AdminAppeals() {
 			}),
 		});
 	};
+	const columns = useMemo(
+		() => [
+			columnHelper.accessor("ban_id", {
+				enableColumnFilter: false,
+				header: "ID",
+				grow: false,
+				size: 30,
+				Cell: ({ cell }) => (
+					<TextLink
+						color={"primary"}
+						to={`/ban/$ban_id`}
+						params={{ ban_id: String(cell.getValue()) }}
+						marginRight={2}
+					>
+						#{cell.getValue()}
+					</TextLink>
+				),
+			}),
+			columnHelper.accessor("appeal_state", {
+				enableColumnFilter: true,
+				header: "Status",
+				size: 150,
+				grow: false,
+				Cell: ({ cell }) => {
+					return <Typography variant={"body1"}>{appealStateString(cell.getValue())}</Typography>;
+				},
+			}),
+			columnHelper.accessor("source_id", {
+				enableColumnFilter: true,
+				header: "Author",
+				grow: true,
+				Cell: ({ row }) => (
+					<PersonCell
+						showCopy={true}
+						steam_id={row.original.source_id}
+						personaname={row.original.source_personaname}
+						avatar_hash={row.original.source_avatarhash}
+					/>
+				),
+			}),
+			columnHelper.accessor("target_id", {
+				enableColumnFilter: true,
+				header: "Subject",
+				grow: true,
+				Cell: ({ row }) => (
+					<PersonCell
+						showCopy={true}
+						steam_id={row.original.target_id}
+						personaname={row.original.target_personaname}
+						avatar_hash={row.original.target_avatarhash}
+					/>
+				),
+			}),
+			columnHelper.accessor("reason", {
+				header: "Reason",
+				size: 150,
+				filterSelectOptions: Object.values(BanReason).map((reason) => ({
+					label: BanReasons[reason],
+					value: reason,
+				})),
+				filterVariant: "multi-select",
+				Cell: ({ cell }) => <Typography>{BanReasons[cell.getValue()]}</Typography>,
+			}),
+			columnHelper.accessor("reason_text", {
+				enableColumnFilter: false,
+				header: "Custom",
+				filterVariant: "text",
+				grow: true,
+				Cell: ({ cell }) => <Typography>{cell.getValue()}</Typography>,
+			}),
+			columnHelper.accessor("created_on", {
+				header: "Created",
+				size: 120,
+				Cell: ({ cell }) => <TableCellRelativeDateField date={cell.getValue()} />,
+			}),
+			columnHelper.accessor("updated_on", {
+				header: "Last Active",
+				enableColumnFilter: false,
+				size: 120,
+				Cell: ({ cell }) => <TableCellRelativeDateField date={cell.getValue()} />,
+			}),
+		],
+		[],
+	);
 
+	const table = useMaterialReactTable({
+		...defaultOptions,
+		columns,
+		data: data ?? [],
+		enableFilters: true,
+		initialState: {
+			...defaultOptions.initialState,
+			sorting: [{ id: "updated_on", desc: true }],
+			columnVisibility: {
+				source_id: false,
+				target_id: true,
+				reason: true,
+				reason_text: true,
+				created_on: false,
+				updated_on: true,
+			},
+		},
+	});
 	return (
 		<Grid container spacing={2}>
 			<Grid size={{ xs: 12 }}>
@@ -184,148 +270,9 @@ function AdminAppeals() {
 
 			<Grid size={{ xs: 12 }}>
 				<ContainerWithHeader title={"Recent Open Appeal Activity"}>
-					<AppealsTable
-						appeals={appeals ?? []}
-						isLoading={isLoading}
-						setColumnFilters={setColumnFilters}
-						columnFilters={columnFilters}
-						setSorting={setSorting}
-						sorting={sorting}
-						setPagination={setPagination}
-						pagination={pagination}
-					/>
+					<MaterialReactTable table={table} />
 				</ContainerWithHeader>
 			</Grid>
 		</Grid>
-		// </Formik>
 	);
 }
-const columnHelper = createColumnHelper<BanRecord>();
-
-const AppealsTable = ({
-	appeals,
-	isLoading,
-	setPagination,
-	pagination,
-	columnFilters,
-	setColumnFilters,
-	sorting,
-	setSorting,
-}: { appeals: BanRecord[]; isLoading: boolean } & TablePropsAll) => {
-	const columns = [
-		columnHelper.accessor("ban_id", {
-			header: "ID",
-			size: 30,
-			cell: (info) => (
-				<TextLink
-					color={"primary"}
-					to={`/ban/$ban_id`}
-					params={{ ban_id: String(info.getValue()) }}
-					marginRight={2}
-				>
-					#{info.getValue()}
-				</TextLink>
-			),
-		}),
-		columnHelper.accessor("appeal_state", {
-			enableColumnFilter: true,
-			filterFn: (row, _, value) => {
-				if (value === AppealState.Any) {
-					return true;
-				}
-				return row.original.appeal_state === value;
-			},
-			header: "Status",
-			size: 85,
-			cell: (info) => {
-				return <Typography variant={"body1"}>{appealStateString(info.getValue())}</Typography>;
-			},
-		}),
-		columnHelper.accessor("source_id", {
-			enableColumnFilter: true,
-			header: "Author",
-			size: 100,
-			cell: (info) => (
-				<PersonCell
-					showCopy={true}
-					steam_id={info.row.original.source_id}
-					personaname={info.row.original.source_personaname}
-					avatar_hash={info.row.original.source_avatarhash}
-				/>
-			),
-		}),
-		columnHelper.accessor("target_id", {
-			enableColumnFilter: true,
-			header: "Subject",
-			size: 100,
-			cell: (info) => (
-				<PersonCell
-					showCopy={true}
-					steam_id={info.row.original.target_id}
-					personaname={info.row.original.target_personaname}
-					avatar_hash={info.row.original.target_avatarhash}
-				/>
-			),
-		}),
-		columnHelper.accessor("reason", {
-			header: "Reason",
-			size: 150,
-			cell: (info) => <Typography>{BanReasons[info.getValue()]}</Typography>,
-		}),
-		columnHelper.accessor("reason_text", {
-			header: "Custom",
-			size: 150,
-			cell: (info) => <Typography>{info.getValue()}</Typography>,
-		}),
-		columnHelper.accessor("created_on", {
-			header: "Created",
-			size: 120,
-			cell: (info) => <Typography>{renderDateTime(info.getValue())}</Typography>,
-		}),
-		columnHelper.accessor("updated_on", {
-			header: "Last Active",
-			size: 120,
-			cell: (info) => <Typography>{renderDateTime(info.getValue())}</Typography>,
-		}),
-	];
-
-	const table = useReactTable({
-		data: appeals,
-		columns: columns,
-		autoResetPageIndex: true,
-		getCoreRowModel: getCoreRowModel(),
-		getFilteredRowModel: getFilteredRowModel(),
-		getPaginationRowModel: getPaginationRowModel(),
-		getSortedRowModel: getSortedRowModel(),
-		onPaginationChange: setPagination,
-		onSortingChange: setSorting,
-		onColumnFiltersChange: setColumnFilters,
-
-		state: {
-			sorting,
-			pagination,
-			columnFilters,
-		},
-	});
-
-	return (
-		<>
-			<DataTable table={table} isLoading={isLoading} />{" "}
-			<PaginatorLocal
-				onRowsChange={(rows) => {
-					setPagination((prev) => {
-						return { ...prev, pageSize: rows };
-					});
-				}}
-				onPageChange={(page) => {
-					setPagination((prev) => {
-						return { ...prev, pageIndex: page };
-					});
-				}}
-				count={table.getRowCount()}
-				rows={pagination.pageSize}
-				page={pagination.pageIndex}
-			/>
-		</>
-	);
-};
