2e5c46359d81f600e7b60626bba7f7c2e440d322
Leigh MacDonald <leigh.macdonald@gmail.com>
2026-03-11 22:44:32 -0600

    Reports table

--- frontend/src/routes/_mod.admin.reports.tsx
+++ frontend/src/routes/_mod.admin.reports.tsx
@@ -1,29 +1,18 @@
-import FilterListIcon from "@mui/icons-material/FilterList";
-import ReportIcon from "@mui/icons-material/Report";
-import ButtonGroup from "@mui/material/ButtonGroup";
 import Grid from "@mui/material/Grid";
-import MenuItem from "@mui/material/MenuItem";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
-import { createFileRoute, useNavigate } from "@tanstack/react-router";
-import { createMRTColumnHelper, MaterialReactTable, useMaterialReactTable } from "material-react-table";
-import { useCallback, useMemo } from "react";
+import { createFileRoute } from "@tanstack/react-router";
+import { createMRTColumnHelper, useMaterialReactTable } from "material-react-table";
+import { useMemo } from "react";
 import { z } from "zod/v4";
 import { apiGetReports } from "../api";
-import { ContainerWithHeader } from "../component/ContainerWithHeader";
 import { PersonCell } from "../component/PersonCell.tsx";
 import { TextLink } from "../component/TextLink.tsx";
 import { createDefaultTableOptions } from "../component/table/options.ts";
+import { SortableTable } from "../component/table/SortableTable.tsx";
 import { TableCellRelativeDateField } from "../component/table/TableCellRelativeDateField.tsx";
-import { useAppForm } from "../contexts/formContext.tsx";
 import { BanReason, BanReasons } from "../schema/bans.ts";
-import {
-	ReportStatus,
-	ReportStatusCollection,
-	type ReportStatusEnum,
-	type ReportWithAuthor,
-	reportStatusString,
-} from "../schema/report.ts";
+import { ReportStatus, type ReportWithAuthor, reportStatusString } from "../schema/report.ts";
 import { commonTableSearchSchema } from "../util/table.ts";
 
 const columnHelper = createMRTColumnHelper<ReportWithAuthor>();
@@ -57,49 +46,11 @@ export const Route = createFileRoute("/_mod/admin/reports")({
 });
 
 function AdminReports() {
-	const navigate = useNavigate({ from: Route.fullPath });
-	const search = Route.useSearch();
 	const { reports } = Route.useLoaderData();
-
-	const form = useAppForm({
-		onSubmit: async ({ value }) => {
-			await navigate({
-				to: "/admin/reports",
-				replace: true,
-				search: (prev) => ({ ...prev, ...value }),
-			});
-		},
-		validators: {
-			onSubmit: z.object({
-				source_id: z.string(),
-				target_id: z.string(),
-				report_status: z.enum(ReportStatus),
-			}),
-		},
-		defaultValues: {
-			source_id: search.source_id ?? "",
-			target_id: search.target_id ?? "",
-			report_status: search.report_status ?? ReportStatus.Any,
-		},
-	});
-
-	const clear = useCallback(async () => {
-		form.reset();
-		await navigate({
-			to: "/admin/reports",
-			search: (prev) => ({
-				...prev,
-				source_id: undefined,
-				target_id: undefined,
-				report_status: undefined,
-			}),
-		});
-	}, [form, navigate]);
 
 	const columns = useMemo(() => {
 		return [
 			columnHelper.accessor("report_id", {
-				enableColumnFilter: false,
 				header: "ID",
 				size: 30,
 				Cell: ({ cell }) => (
@@ -114,13 +65,15 @@ function AdminReports() {
 				),
 			}),
 			columnHelper.accessor("report_status", {
-				size: 120,
 				header: "Status",
 				filterVariant: "multi-select",
 				filterSelectOptions: Object.values(ReportStatus).map((status) => ({
 					label: reportStatusString(status),
 					value: status,
 				})),
+				filterFn: (row, _, filterValue) => {
+					return filterValue.length === 0 || filterValue.includes(row.original.report_status);
+				},
 				Cell: ({ cell }) => {
 					return (
 						<Stack direction={"row"} spacing={1}>
@@ -132,6 +85,22 @@ function AdminReports() {
 			columnHelper.accessor("source_id", {
 				header: "Reporter",
 				grow: true,
+				enableColumnFilter: true,
+				filterFn: (row, _, filterValue) => {
+					const query = filterValue.toLowerCase();
+					if (query === "") {
+						return true;
+					}
+					const value = row.original.source_id.toLowerCase();
+					if (value.includes(query)) {
+						return true;
+					}
+					if (row.original.source_id.includes(query) || row.original.source_id === query) {
+						return true;
+					}
+
+					return false;
+				},
 				Cell: ({ row }) => (
 					<PersonCell
 						showCopy={true}
@@ -144,6 +113,22 @@ function AdminReports() {
 			columnHelper.accessor("target_id", {
 				header: "Subject",
 				grow: true,
+				enableColumnFilter: true,
+				filterFn: (row, _, filterValue) => {
+					const query = filterValue.toLowerCase();
+					if (query === "") {
+						return true;
+					}
+					const value = row.original.target_id.toLowerCase();
+					if (value.includes(query)) {
+						return true;
+					}
+					if (row.original.target_id.includes(query) || row.original.target_id === query) {
+						return true;
+					}
+
+					return false;
+				},
 				Cell: ({ row }) => (
 					<PersonCell
 						showCopy={true}
@@ -154,25 +139,32 @@ function AdminReports() {
 				),
 			}),
 			columnHelper.accessor("reason", {
-				enableColumnFilter: false,
 				filterSelectOptions: Object.values(BanReason).map((reason) => ({
 					label: BanReasons[reason],
 					value: reason,
 				})),
+				filterVariant: "multi-select",
 				header: "Reason",
-				size: 100,
+				size: 150,
+				filterFn: (row, _, filterValue) => {
+					return (
+						filterValue.length === 0 ||
+						filterValue.includes(BanReason.Any) ||
+						filterValue.includes(row.original.reason)
+					);
+				},
 				Cell: ({ cell }) => <Typography>{BanReasons[cell.getValue()]}</Typography>,
 			}),
 			columnHelper.accessor("created_on", {
-				enableColumnFilter: false,
-				size: 100,
 				header: "Created",
+				size: 150,
+				filterVariant: "date",
 				Cell: ({ cell }) => <TableCellRelativeDateField date={cell.getValue()} />,
 			}),
 			columnHelper.accessor("updated_on", {
-				enableColumnFilter: false,
-				size: 100,
 				header: "Updated",
+				size: 150,
+				filterVariant: "date",
 				Cell: ({ cell }) => <TableCellRelativeDateField date={cell.getValue()} />,
 			}),
 		];
@@ -185,7 +177,6 @@ function AdminReports() {
 		enableFilters: true,
 		initialState: {
 			...defaultOptions.initialState,
-
 			sorting: [{ id: "updated_on", desc: true }],
 			columnVisibility: {
 				source_id: false,
@@ -202,70 +193,7 @@ function AdminReports() {
 	return (
 		<Grid container spacing={2}>
 			<Grid size={{ xs: 12 }}>
-				<ContainerWithHeader title={"Filters"} iconLeft={<FilterListIcon />} marginTop={2}>
-					<form
-						onSubmit={async (e) => {
-							e.preventDefault();
-							e.stopPropagation();
-							await form.handleSubmit();
-						}}
-					>
-						<Grid container spacing={2}>
-							<Grid size={{ xs: 6, md: 4 }}>
-								<form.AppField
-									name={"source_id"}
-									children={(field) => {
-										return <field.SteamIDField label={"Author Steam ID"} />;
-									}}
-								/>
-							</Grid>
-
-							<Grid size={{ xs: 6, md: 4 }}>
-								<form.AppField
-									name={"target_id"}
-									children={(field) => {
-										return <field.SteamIDField label={"Subject Steam ID"} />;
-									}}
-								/>
-							</Grid>
-
-							<Grid size={{ xs: 6, md: 4 }}>
-								<form.AppField
-									name={"report_status"}
-									children={(field) => {
-										return (
-											<field.SelectField
-												label={"Report Status"}
-												fullWidth
-												items={ReportStatusCollection}
-												renderItem={(item) => {
-													return (
-														<MenuItem value={item} key={`rs-${item}`}>
-															{reportStatusString(item as ReportStatusEnum)}
-														</MenuItem>
-													);
-												}}
-											/>
-										);
-									}}
-								/>
-							</Grid>
-							<Grid size={{ xs: 12 }}>
-								<form.AppForm>
-									<ButtonGroup>
-										<form.ResetButton onClick={clear} />
-										<form.SubmitButton />
-									</ButtonGroup>
-								</form.AppForm>
-							</Grid>
-						</Grid>
-					</form>
-				</ContainerWithHeader>
-			</Grid>
-			<Grid size={{ xs: 12 }}>
-				<ContainerWithHeader title={"Current User Reports"} iconLeft={<ReportIcon />}>
-					<MaterialReactTable table={table} />
-				</ContainerWithHeader>
+				<SortableTable table={table} title={"User Reports"} />
 			</Grid>
 		</Grid>
 	);
