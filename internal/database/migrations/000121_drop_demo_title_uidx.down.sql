88b0fc20af1698b9799832c9630c12a6337d4f2b
Leigh MacDonald <leigh.macdonald@gmail.com>
2026-07-19 17:17:42 -0600

    Fix demo_id col

--- internal/database/migrations/000121_drop_demo_title_uidx.down.sql
+++ internal/database/migrations/000121_drop_demo_title_uidx.down.sql
@@ -1,10 +1,10 @@
 CREATE UNIQUE INDEX IF NOT EXISTS demo_title_uindex ON demos (title);
 
 ALTER TABLE ban
-ADD COLUMN IF NOT EXISTS demo_id int references demo (demo_id);
+DROP COLUMN IF EXISTS anticheat_id;
 
 ALTER TABLE ban
-DROP COLUMN IF EXISTS anticheat_id;
+DROP COLUMN IF EXISTS demo_id;
 
 ALTER TABLE person_messages
 DROP COLUMN IF EXISTS demo_id;
--- internal/database/migrations/000121_drop_demo_title_uidx.up.sql
+++ internal/database/migrations/000121_drop_demo_title_uidx.up.sql
@@ -1,10 +1,10 @@
 DROP INDEX IF EXISTS demo_title_uindex;
 
 ALTER TABLE ban
-DROP COLUMN IF EXISTS demo_id;
+ADD COLUMN IF NOT EXISTS anticheat_id bigint references anticheat (anticheat_id);
 
 ALTER TABLE ban
-ADD COLUMN IF NOT EXISTS anticheat_id bigint references anticheat (anticheat_id);
+ADD COLUMN IF NOT EXISTS demo_id integer references demo (demo_id);
 
 ALTER TABLE person_messages
 ADD COLUMN IF NOT EXISTS demo_id integer references demo (demo_id);
