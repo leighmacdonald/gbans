be01f558aa7b5da9e5cb18edf196d7ea28c5a971
Leigh MacDonald <leigh.macdonald@gmail.com>
2026-07-22 20:58:11 -0600

    wip

--- internal/discord/oauth/discord_oauth_service.go
+++ internal/discord/oauth/discord_oauth_service.go
@@ -6,22 +6,33 @@ import (
 	"log/slog"
 
 	"connectrpc.com/connect"
+	"github.com/leighmacdonald/gbans/internal/auth/permission"
 	"github.com/leighmacdonald/gbans/internal/database"
 	v1 "github.com/leighmacdonald/gbans/internal/discord/oauth/v1"
+	"github.com/leighmacdonald/gbans/internal/discord/oauth/v1/oauthv1connect"
 	"github.com/leighmacdonald/gbans/internal/rpc"
 	"google.golang.org/protobuf/types/known/emptypb"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 type DiscordService struct {
-	discord DiscordOAuth
+	discordOAuth DiscordOAuth
+}
+
+func NewService(discord DiscordOAuth, authMiddleware *rpc.Middleware, option ...connect.HandlerOption) rpc.Service {
+	pattern, handler := oauthv1connect.NewDiscordOAuthServiceHandler(&DiscordService{discordOAuth: discord}, option...)
+	authMiddleware.UserRoute(oauthv1connect.DiscordOAuthServiceLoginProcedure, rpc.WithMinPermissions(permission.User))
+	authMiddleware.UserRoute(oauthv1connect.DiscordOAuthServiceLogoutProcedure, rpc.WithMinPermissions(permission.User))
+	authMiddleware.UserRoute(oauthv1connect.DiscordOAuthServiceProfileProcedure, rpc.WithMinPermissions(permission.User))
+
+	return rpc.Service{Pattern: pattern, Handler: handler}
 }
 
 func (s *DiscordService) Login(ctx context.Context, _ *emptypb.Empty) (*v1.LoginResponse, error) {
 	currentUser := rpc.UserInfoFromCtx(ctx)
 	sid := currentUser.GetSteamID()
 
-	loginURL, errURL := s.discord.CreateStatefulLoginURL(sid)
+	loginURL, errURL := s.discordOAuth.CreateStatefulLoginURL(sid)
 	if errURL != nil {
 		return nil, connect.NewError(connect.CodeInvalidArgument, rpc.ErrBadRequest)
 	}
@@ -34,7 +45,7 @@ func (s *DiscordService) Login(ctx context.Context, _ *emptypb.Empty) (*v1.Login
 func (s *DiscordService) Logout(ctx context.Context, _ *emptypb.Empty) (*emptypb.Empty, error) {
 	user := rpc.UserInfoFromCtx(ctx)
 
-	errUser := s.discord.Logout(ctx, user.GetSteamID())
+	errUser := s.discordOAuth.Logout(ctx, user.GetSteamID())
 	if errUser != nil {
 		if errors.Is(errUser, database.ErrNoResult) {
 			return &emptypb.Empty{}, nil
@@ -48,7 +59,7 @@ func (s *DiscordService) Logout(ctx context.Context, _ *emptypb.Empty) (*emptypb
 
 func (s *DiscordService) Profile(ctx context.Context, _ *emptypb.Empty) (*v1.ProfileResponse, error) {
 	user := rpc.UserInfoFromCtx(ctx)
-	discord, errUser := s.discord.GetUserDetail(ctx, user.GetSteamID())
+	discord, errUser := s.discordOAuth.GetUserDetail(ctx, user.GetSteamID())
 	if errUser != nil {
 		if errors.Is(errUser, database.ErrNoResult) {
 			return nil, connect.NewError(connect.CodeNotFound, rpc.ErrNotFound)
